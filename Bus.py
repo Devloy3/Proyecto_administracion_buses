08dc7fade4fee224d82b2562dadcaa07e7b1bef5
Eloy <eloydominguez3@gmail.com>
2025-09-19 18:57:48 +0200

    02_eloy_Creando la classe

--- Bus.py
+++ Bus.py
@@ -1,16 +1,23 @@
-from Billete import billetes 
-
 class bus:
     def __init__(self):
-        self.billete = billetes()
         self.__plazas = 100
+        self.__plaza_disponible = self.__plazas
 
-    def añadir_plaza(self):
-        if self.billete:
-            self.disponible = self.__plazas - 1
-            
-            if self.disponible == 0:
-                print("Se acabaron las plazas")
-
-    
+    def añadir_plaza(self,cantidad_de_billetes):
+        if self.__plaza_disponible >= cantidad_de_billetes:
+            self.__plazas_disponible -= cantidad_de_billetes
+            texto_2 = f"Quedan {self.__plaza_disponible} plazas"
+            return texto_2
+        else:
+            texto = "No hay plazas, crack"
+            return texto 
+        
+    def añadir_plaza(self,cantidad_de_billetes):
+        if self.__plaza_disponible <= cantidad_de_billetes:
+            self.__plazas_disponible += cantidad_de_billetes
+            texto_2 = f"Hay{self.__plaza_disponible} plazas"
+            return texto_2
+        else:
+            texto = "No hay plazas de ha devolver, estupido"
+            return texto 
         
