329f37f0edc361b0eeb9f7f7385b722fc65121a1
Eloy <eloydominguez3@gmail.com>
2025-09-19 18:59:36 +0200

    03_eloy

--- Bus.py
+++ Bus.py
@@ -3,7 +3,7 @@ class bus:
         self.__plazas = 100
         self.__plaza_disponible = self.__plazas
 
-    def añadir_plaza(self,cantidad_de_billetes):
+    def comprar_plaza(self,cantidad_de_billetes):
         if self.__plaza_disponible >= cantidad_de_billetes:
             self.__plazas_disponible -= cantidad_de_billetes
             texto_2 = f"Quedan {self.__plaza_disponible} plazas"
@@ -12,12 +12,12 @@ class bus:
             texto = "No hay plazas, crack"
             return texto 
         
-    def añadir_plaza(self,cantidad_de_billetes):
+    def reembolsar_plazas(self,cantidad_de_billetes):
         if self.__plaza_disponible <= cantidad_de_billetes:
             self.__plazas_disponible += cantidad_de_billetes
             texto_2 = f"Hay{self.__plaza_disponible} plazas"
             return texto_2
         else:
-            texto = "No hay plazas de ha devolver, estupido"
+            texto = "No hay plazas ha devolver, estupido"
             return texto 
         
