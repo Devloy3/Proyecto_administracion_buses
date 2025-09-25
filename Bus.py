0eb090dbf964851654d9b9992683950ecb575864
AnomalousScripts <abelalejandrovaldes22gmail.com>
2025-09-25 17:59:10 +0200

    Cambios Abel

--- Bus.py
+++ Bus.py
@@ -1,11 +1,14 @@
+from Billete import Billete
+
 class bus:
-    def __init__(self):
-        self.__plazas = 100
+    def __init__(self,plazas):
+        self.__plazas = plazas
         self.__plaza_disponible = self.__plazas
+        Billetes_Vendidos = []
 
     def comprar_plaza(self,cantidad_de_billetes):
         if self.__plaza_disponible >= cantidad_de_billetes:
-            self.__plazas_disponible -= cantidad_de_billetes
+            self.__plazas_disponible -= 1
             texto_2 = f"Quedan {self.__plaza_disponible} plazas"
             return texto_2
         else:
@@ -14,10 +17,14 @@ class bus:
         
     def reembolsar_plazas(self,cantidad_de_billetes):
         if self.__plaza_disponible <= cantidad_de_billetes:
-            self.__plazas_disponible += cantidad_de_billetes
+            self.__plazas_disponible += 1
             texto_2 = f"Hay{self.__plaza_disponible} plazas"
             return texto_2
         else:
             texto = "No hay plazas ha devolver, estupido"
             return texto 
-        
+    
+    def mostrar_venta(self):
+        print(f"Total: {self.__plazas}")
+        print(f"Libre: {self.__plaza_disponible}")
+        print(f"Vendido: {self.__plazas - self.__plaza_disponible }")
